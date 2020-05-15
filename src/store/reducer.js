4d8e01becf92780e1668277b0a8096efc1a1b811
Abhishek Sharma <sharmaash876@gmail.com>
2020-05-15 23:41:48 +0530

    Using Redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,14 @@
 const initialState = {
-    counter : 0
+    counter: 0
 }
 
-const reducer = (state = initialState , action) => {
-    return {
-        state
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
     }
-}
+    return state;
+};
 
-export default reducer
+export default reducer;
